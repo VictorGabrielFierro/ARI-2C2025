f173b1d01883b204a9437f2191e9af2f54458ba9
Gabriel Fierro <vigabf@gmai.com>
2025-11-21 14:34:16 -0300

    Borre el proyecto original de la catedra para poder empezar con el nuestro

--- recursos/creacion-db.sql
+++
@@ -1,6 +0,0 @@
-create user aida_owner nologin;
-create user aida_admin password 'cambiar_esta_clave';
-
-create database aida_db owner aida_owner;
-grant connect on database aida_db to aida_admin;
-
--- recursos/ejemplo-local-sets.bat
+++
@@ -1,5 +0,0 @@
-set PGUSER=aida_admin
-set PGPASSWORD=cambiar_esta_clave
-set PGHOST=localhost
-set PGPORT=5432
-set PGDATABASE=aida_db
